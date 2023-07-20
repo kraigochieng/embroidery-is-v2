3aad4968eacd70edab02c6d85861d2715f376ecd
kraigochieng <kraigochieng@gmail.com>
2023-07-20 10:52:45 +0300

    Changed the RequestMapping for ItemController

--- server/src/main/java/com/kraigochieng/embroideryis/server/item/ItemController.java
+++ server/src/main/java/com/kraigochieng/embroideryis/server/item/ItemController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/admin/colours")
+@RequestMapping(path = "api/admin/items")
 @CrossOrigin
 public class ItemController {
     private final ItemService itemService;
