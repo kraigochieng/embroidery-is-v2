e3380c982dfaad122b045e86207a33a20174fdaf
kraigochieng <kraigochieng@gmail.com>
2023-07-26 09:52:57 +0300

    Added classnames for styling done in index.css

--- client/src/components/navbars/AdminNavbar.jsx
+++ client/src/components/navbars/AdminNavbar.jsx
@@ -4,9 +4,10 @@ import { Link, Outlet } from 'react-router-dom'
 export default function AdminNavbar() {
   return (
     <div>
-        <ul>
+        <ul className="navbar">
             <li><Link to="/admin/users">Users</Link></li>
             <li><Link to="/admin/store">Store</Link></li>
+            <li><Link to="/">Back To Home</Link></li>
         </ul>
         <Outlet />
     </div>
--- client/src/components/navbars/StoreNavbar.jsx
+++ client/src/components/navbars/StoreNavbar.jsx
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom'
 export default function StoreNavbar() {
   return (
     <div>
-        <ul>
+        <ul className="navbar">
             <li><Link to="items">Items</Link></li>
             <li><Link to="colours">Colours</Link></li>
             <li><Link to="positions">Positions</Link></li>
