bdd132f36811da4af1da451140c4895db82df3ae
kraigochieng <kraigochieng@gmail.com>
2023-07-31 13:39:30 +0300

    Added logout button and redirect functionality to go to homepage after logout

--- client/src/components/navbars/HomeNavbar.jsx
+++ client/src/components/navbars/HomeNavbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import {Link, Outlet } from 'react-router-dom'
+import {Link, Outlet, useNavigate } from 'react-router-dom'
 export default function HomeNavbar() {
+
+  const navigate = useNavigate()
+
+  function handleLogOut() {
+    localStorage.removeItem("jwt")
+    navigate("/")
+  }
   return (
     <div>
       <ul className="navbar">
         <li><Link to="/admin">Admin</Link></li>
       </ul>
-        <Outlet />
+      <button onClick={handleLogOut}>Log Out</button>
+      <Outlet />
     </div>
     
   )
