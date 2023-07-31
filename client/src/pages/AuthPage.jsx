d60320f2276b736b66fe7561d5608438f73cd9cc
kraigochieng <kraigochieng@gmail.com>
2023-07-31 13:44:02 +0300

    Created AuthPage

---
+++ client/src/pages/AuthPage.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { auth } from '../axiosInstances'
+import { useNavigate } from 'react-router-dom'
+
+export default function AuthPage() {
+    const navigate = useNavigate()
+    const [loginFormData, setLoginFormData] = useState({
+        username: "kraig16",
+        password: "password"
+    })
+
+    function handleLoginFormChange(event) {
+        const {name, value} = event.target
+        setLoginFormData(prevLoginFormData => ({
+            ...prevLoginFormData,
+            [name]: value
+        }))
+    }
+
+
+    function handleLoginFormSubmit(event) {
+        event.preventDefault()
+        auth.post("login", loginFormData)
+            .then(response => {
+                // set JWT token
+                localStorage.setItem("jwt", response.data)
+                //
+                navigate("/")
+            })
+            .catch(error => {
+                console.error(error.response.status)
+                alert("Either username or password is invalid")
+            })
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleLoginFormSubmit}>
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={loginFormData.username}
+                onChange={handleLoginFormChange}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={loginFormData.password}
+                onChange={handleLoginFormChange}
+            />
+            <button>Login</button>
+        </form>
+    </div>
+  )
+}
