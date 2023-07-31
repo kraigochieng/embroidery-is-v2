71cfb5702c8a2d22e7d8b53bb407fd4ea1329ac1
kraigochieng <kraigochieng@gmail.com>
2023-07-31 13:42:03 +0300

    Added redirect functionality to login page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 export default function HomePage() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    // Go to login if jwt token is not set
+    if(!localStorage.getItem("jwt")) {
+      navigate("/auth")
+    }
+  })
   return (
-    <div className="page">HomePage</div>
+    <>
+      <div className="page">HomePage</div>
+    </>
+    
   )
 }
