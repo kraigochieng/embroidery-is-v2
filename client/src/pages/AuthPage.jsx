98e42fc5ea65629c9f1bd8a99c0a8a46cfd6a1f8
kraigochieng <kraigochieng@gmail.com>
2023-09-11 18:17:05 +0300

    Added loading component

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import { auth } from '../axiosInstances'
 import { useNavigate } from 'react-router-dom'
 import { Alert, Button, Form, Input } from 'antd'
+import Loading from '../components/Loading'
 export default function AuthPage() {
     // State
     const [loginForm] = Form.useForm() // to set default values
     const [isUserValid, setIsUserValid] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
+
     // Form Config
     loginForm.setFieldsValue({
         username: 'kraig',
@@ -17,6 +20,7 @@ export default function AuthPage() {
 
     // Form Submission
     function handleFinish(values) {
+        setIsLoading(true)
         auth.post('login', values)
             .then(response => {
                 sessionStorage.setItem("jwt", response.data.token) // Set JWT Token
@@ -25,6 +29,7 @@ export default function AuthPage() {
             })
             .catch(error => {
                 setIsUserValid(false)
+                setIsLoading(false)
                 console.error(error.message)
             })
     }
@@ -36,24 +41,29 @@ export default function AuthPage() {
 
     return (
         <>
-            <Form form={loginForm} name='loginForm' onFinish={handleFinish}>
-                <Form.Item label='Username' name='username'>
-                    <Input placeholder='Username'/>
-                </Form.Item>
-                <Form.Item label='Password' name='password'>
-                    <Input.Password placeholder='Password'/>
-                </Form.Item>
-                <Button htmlType='submit'>Login</Button>
-                {
-                    !isUserValid &&
-                    <Alert
-                        type='error'
-                        message='Either username or password invalid'
-                        closable
-                        onClose={handleClose}
-                    />
-                }
-            </Form>
+            {
+                isLoading ?
+                <Loading /> :
+                <Form form={loginForm} name='loginForm' onFinish={handleFinish}>
+                    <Form.Item label='Username' name='username'>
+                        <Input placeholder='Username'/>
+                    </Form.Item>
+                    <Form.Item label='Password' name='password'>
+                        <Input.Password placeholder='Password'/>
+                    </Form.Item>
+                    <Button htmlType='submit'>Login</Button>
+                    {
+                        !isUserValid &&
+                        <Alert
+                            type='error'
+                            message='Either username or password invalid'
+                            closable
+                            onClose={handleClose}
+                        />
+                    }
+                </Form>
+            }
+
         </>
     )
 }
